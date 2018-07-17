b09fb9605890dde6ff5abd4e455aadaae4abba18
132yse <1533540012@qq.com>
2018-07-17 12:44:32 +0800

    完成基本架构

---
+++ api/dbOpt/api.go
@@ -0,0 +1,17 @@
+package dbOpt
+
+import (
+	"database/sql"
+)
+
+func openConn() *sql.DB{
+
+}
+
+func CreateUserCredential(loginNmae:string, pwd:string) error {
+
+}
+
+func GetUserCredential(loginNmae:string) (string,error) {
+
+}
--- api/def/apidef.go
+++ api/def/apidef.go
@@ -5,4 +5,13 @@ type UserCredential struct {
 	Pwd string `json:"pwd"`
 	Role string `json:"role"`
 	QQ string `json:"qq"`
+	Desc string `json:"desc"`
+}
+
+type Post struct {
+	Title string `json:"title"`
+	Status string `json:"status"`
+	Sort string `json:"sort"`
+	Time int `json:"time"`
+	Author UserCredential `json:"author"`
 }
