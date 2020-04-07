f69798c0306ca0aed6ebf7e4983821266b304c16
132yse <1533540012@qq.com>
2020-04-07 10:19:47 +0800

    增加replace接口

--- db/count.go
+++ db/count.go
@@ -7,10 +7,6 @@ import (
 
 func GetPv(pid int) (*def.Pv, error) {
 	stmtCount, err := dbConn.Prepare("SELECT pv FROM pv WHERE pid = ?")
-	if err != nil {
-		return nil, err
-	}
-
 	var pv int
 	err = stmtCount.QueryRow(pid).Scan(&pv)
 	if err != nil && err != sql.ErrNoRows {
@@ -20,6 +16,21 @@ func GetPv(pid int) (*def.Pv, error) {
 	res := &def.Pv{Pid: pid, Pv: pv}
 
 	defer stmtCount.Close()
+	return res, nil
+}
 
+func ReplacePv(pid int, pv int) (*def.Pv, error) {
+	stmtIns, err := dbConn.Prepare("REPLACE INTO pv (pid,pv) VALUES (?,?)")
+	if err != nil {
+		return nil, err
+	}
+	_, err = stmtIns.Exec(pid, pv)
+	if err != nil {
+		return nil, err
+	}
+	defer stmtIns.Close()
+
+	res := &def.Pv{Pid: pid, Pv: pv}
+	defer stmtIns.Close()
 	return res, nil
 }
